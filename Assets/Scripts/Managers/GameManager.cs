bab3411e741ce03db5aa9216b85708edd551a994
miggysy <123222299+miggysy@users.noreply.github.com>
2023-07-18 12:26:09 +0800

    Add lives manager functionality

--- Assets/Scripts/Managers/GameManager.cs
+++ Assets/Scripts/Managers/GameManager.cs
@@ -4,15 +4,10 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    public delegate void GameEvent();
+    public static GameEvent onOrderSubmitted;   //Call when an order was submitted
+    public static GameEvent onOrderCorrect; //Call when the order submitted was correct
+    public static GameEvent onOrderWrong;   //Call when the order submitted was wrong
+    public static GameEvent onCustomerLeft; //Call when the customer's timer runs out without getting an order
+    public static GameEvent onGameOver; //Call when the player runs out of lives
 }
--- Assets/Scripts/Managers/LivesManager.cs
+++ Assets/Scripts/Managers/LivesManager.cs
@@ -1,18 +1,47 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 public class LivesManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private int totalLives;
+    [SerializeField] private int currentLives;
+    public static event Action onInitializeLives;
+    public static event Action onLoseLife;
+
+    private void Start()
     {
-        
+        InitializeLives();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void InitializeLives() 
     {
-        
+        currentLives = totalLives;
+        onInitializeLives?.Invoke();
+    }
+
+    private void LoseLife() 
+    {
+        currentLives--;
+        CheckGameOver();
+        onLoseLife?.Invoke();
+    }
+
+    private void CheckGameOver()
+    {
+        if(currentLives <= 0) GameManager.onGameOver?.Invoke();
+    }
+
+    private void OnEnable()
+    {
+        GameManager.onCustomerLeft += LoseLife;
+        GameManager.onOrderWrong += LoseLife;
+    }
+
+    private void OnDisable()
+    {
+        GameManager.onCustomerLeft -= LoseLife;
+        GameManager.onOrderWrong -= LoseLife;
     }
 }
